8d33ad219023be3ca9e117bddc27c8259e4bdd5b
rbarbaresco <barbaresco.rodrigo@gmail.com>
2015-06-09 20:46:41 -0300

    Second commit

--- src/br/univali/pdi/Main.java
+++ src/br/univali/pdi/Main.java
@@ -1,9 +1,62 @@
 package br.univali.pdi;
 
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import java.io.File;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.highgui.VideoCapture;
+
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws Exception {
+		
+		nu.pattern.OpenCV.loadShared();
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+		
+		Mat frame = new Mat();
+		VideoCapture camera = new VideoCapture("files/08-amp.mp4");
+		JFrame jframe = new JFrame("Title");
+		jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JLabel vidPanel = new JLabel();
+		jframe.setContentPane(vidPanel);
+		jframe.setVisible(true);
+		
+		while (true) {
+			
+			if (camera.read(frame)) {
+				
+				ImageIcon image = new ImageIcon(Mat2BufferedImage(frame));
+				vidPanel.setIcon(image);
+				vidPanel.repaint();
+				
+			}
+			
+		}
+	}
+
+	public static BufferedImage Mat2BufferedImage(Mat m) {
+		// source:
+		// http://answers.opencv.org/question/10344/opencv-java-load-image-to-gui/
+		// Fastest code
+		// The output can be assigned either to a BufferedImage or to an Image
+
+		int type = BufferedImage.TYPE_BYTE_GRAY;
+		if (m.channels() > 1) {
+			type = BufferedImage.TYPE_3BYTE_BGR;
+		}
+		int bufferSize = m.channels() * m.cols() * m.rows();
+		byte[] b = new byte[bufferSize];
+		m.get(0, 0, b); // get all the pixels
+		BufferedImage image = new BufferedImage(m.cols(), m.rows(), type);
+		final byte[] targetPixels = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
+		System.arraycopy(b, 0, targetPixels, 0, b.length);
+		return image;
 
 	}
 
