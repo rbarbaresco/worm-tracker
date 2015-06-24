a015340f554aa491c73e19062038536b251ee8c8
Thiago Pasquali Ramos <thpramos@gmail.com>
2015-06-23 21:52:37 -0300

    Conseguimos!

--- src/br/univali/pdi/Main.java
+++ src/br/univali/pdi/Main.java
@@ -8,6 +8,8 @@ import java.awt.image.Raster;
 import java.awt.image.WritableRaster;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import javax.swing.ImageIcon;
@@ -17,6 +19,7 @@ import javax.swing.JLabel;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint;
+import org.opencv.core.Point;
 import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 import org.opencv.core.Size;
@@ -33,6 +36,7 @@ public class Main {
 		
 		Mat frame = new Mat();
 		VideoCapture camera = new VideoCapture("files/08-amp.mp4");
+//		VideoCapture camera = new VideoCapture("files/05-dec-amplitude.mp4");
 		JFrame jframe = new JFrame("Title");
 		jframe.setExtendedState(JFrame.MAXIMIZED_BOTH);
 		jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -54,14 +58,19 @@ public class Main {
 		
 		BackgroundSubtractorMOG2 bgs = new BackgroundSubtractorMOG2();
 		
+		Rect bounding_rect = null;
+		int delay = 10;
 		while (true) {
 			
 			if (camera.read(frame)) {
-				Imgproc.cvtColor(frame, frame, Imgproc.COLOR_RGB2GRAY);
-				Imgproc.threshold(frame, frame, 65, 255, Imgproc.THRESH_BINARY_INV);
+				Mat frameEd = new Mat();
+
+				Imgproc.cvtColor(frame, frameEd, Imgproc.COLOR_RGB2GRAY);
+				Imgproc.threshold(frameEd, frameEd, 65, 255, Imgproc.THRESH_BINARY_INV);
 				//Imgproc.inv
-				
-				Imgproc.dilate(frame, frame, Imgproc.getStructuringElement(Imgproc.MORPH_DILATE, new Size(8,8)));
+				Imgproc.blur(frameEd, frameEd, new Size(3,3));
+				Imgproc.threshold(frameEd, frameEd, 10, 255, Imgproc.THRESH_BINARY);
+				Imgproc.dilate(frameEd, frameEd, Imgproc.getStructuringElement(Imgproc.MORPH_DILATE, new Size(8,8)));
 				List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
 				Mat hierarchy = new Mat();
 				//alphaBGS(bi);
@@ -70,30 +79,73 @@ public class Main {
 				//di.draw(bi);
 				
 				
-				Imgproc.findContours(frame, contours, hierarchy,
+				Imgproc.findContours(frameEd, contours, hierarchy,
 						Imgproc.RETR_CCOMP, Imgproc.CHAIN_APPROX_SIMPLE);
 
 				double largest_area = 0;
 				int largest_contour_index = 0;
-				Rect bounding_rect;
-				for (int i = 0; i < contours.size(); i++) {
-					double a = Imgproc.contourArea(contours.get(i), false);
-					if (a > largest_area) {
-						largest_area = a;
-						largest_contour_index = i;
-						bounding_rect = Imgproc.boundingRect(contours.get(i));
+				
+				Collections.sort(contours,new Comparator<MatOfPoint>() {
+
+					@Override
+					public int compare(MatOfPoint o1, MatOfPoint o2) {
+						double a = Imgproc.contourArea(o1, false);
+						double b = Imgproc.contourArea(o2, false);
+
+						return a < b ? 1 : -1;
 					}
+				});
+				
+				if(delay <= 0 && bounding_rect != null){
+					for (int i = 0; i < contours.size(); i++) {
+						Rect new_rect = Imgproc.boundingRect(contours.get(i));
+						if (inArea(bounding_rect, new_rect)) {
+							bounding_rect =  Imgproc.boundingRect(contours.get(i));
+							largest_area = Imgproc.contourArea(contours.get(i), false);
+							largest_contour_index = i;
+							break;
+						}
+					}
+				}else{
+					bounding_rect =  Imgproc.boundingRect(contours.get(0));
 				}
 				
-				Imgproc.cvtColor(frame, frame, Imgproc.COLOR_GRAY2RGB);
+				
+//				for (int i = 0; i < contours.size(); i++) {
+//					
+//					double a = Imgproc.contourArea(contours.get(i), false);
+//					
+//					Rect new_bounding = Imgproc.boundingRect(contours.get(i));
+//					
+////					if ( delay <= 0 && bounding_rect != null && !inArea(bounding_rect,new_bounding) ) {
+////						continue;
+////					}
+//					if ( a > largest_area ) {
+//						largest_area = a;
+//						largest_contour_index = i;
+//						bounding_rect = new_bounding;
+//					}
+//				}
+				
+				delay--;
+				
+				Point center1 = new Point(bounding_rect.x + bounding_rect.width/2,bounding_rect.y + bounding_rect.height/2);
 
-				Scalar color = new Scalar(255, 0, 0);
+				System.out.println(center1+" : "+largest_area+" : "+delay);
+				Imgproc.cvtColor(frameEd, frameEd, Imgproc.COLOR_GRAY2RGB);
+
+				Scalar colorR = new Scalar(255, 0, 0);
+				Scalar colorG = new Scalar(0, 255, 0);
 
 				ArrayList<MatOfPoint> arrayList = new ArrayList<MatOfPoint>();
 				arrayList.add(contours.get(largest_contour_index));
 
-				Imgproc.drawContours( frame, arrayList,0, color,-1 ); // Draw the largest contour using previously stored index.
+				Imgproc.drawContours( frame, arrayList,0, colorR,3 ); // Draw the largest contour using previously stored index.
 				
+				
+				Core.circle(frame, center1, Math.max(bounding_rect.width, bounding_rect.height)/2, colorG);
+				Core.rectangle(frame, new Point(bounding_rect.x,bounding_rect.y), 
+						new Point(bounding_rect.x+bounding_rect.width,bounding_rect.y+bounding_rect.height), colorG, 3); 
 				//Imgproc.rectangle(src, bounding_rect,  Scalar(0,255,0),1, 8,0); 
 				
 				BufferedImage bi = Mat2BufferedImage(frame);
@@ -104,6 +156,16 @@ public class Main {
 			}
 			
 		}
+	}
+
+	private static int span = 100;
+	
+	private static boolean inArea(Rect rect, Rect new_rect) {
+		Point center1 = new Point(rect.x + rect.width/2,rect.y + rect.height/2);
+		Point center_new = new Point(new_rect.x + new_rect.width/2,new_rect.y + new_rect.height/2);
+		double dist = Math.sqrt(Math.pow(center1.x-center_new.x, 2)+Math.pow(center1.y-center_new.y, 2));
+		
+		return dist <= span;
 	}
 
 	public static BufferedImage Mat2BufferedImage(Mat m) {
